2d1a2bb99b7f5391580961c2ab757d8dfe18f4d7
erefy <EREFY@mizutani-no-MacBook-Air.local>
2013-03-18 01:25:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushServer::Application.config.secret_token = '05c862f58cd46d7db1e7faa3c7d1442ec6b382ff858e1b6427e2c2ea55c4be463a8a9cddb252925e82bf5c7472ba2b22ba98b008125b501da3ec284b0af0bfda'
