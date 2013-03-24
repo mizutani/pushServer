6610a7a3827ac5f0d80638481acb5fc422f20b56
erefy <EREFY@erefy.local>
2013-03-24 12:56:53 +0900

    test

--- app/controllers/app_users_controller.rb
+++ app/controllers/app_users_controller.rb
@@ -26,13 +26,13 @@ class AppUsersController < ApplicationController
   # POST /app_users
   # POST /app_users.json
   def create
-    @app_user = @app.app_users.build(params[:app_user])
+    @app_user = @app.app_users.build(app_params)
     @app_user.app_id = @app.id
     @app_user.app_name = @app.name
     respond_to do |format|
       if @app_user.save
         format.html { redirect_to ({action:'show', id:@app_user.id}), notice: 'App user was successfully created.' }
-        format.json { render json: @app_user.json, status: :created, location: @app_user }
+          format.json { render json: @app_user, status: :created, location: {:action => 'show', id:@app_user.id} }
       else
         format.html { render action: "new" }
         format.json { render json: @app_user.errors, status: :unprocessable_entity }
@@ -72,7 +72,7 @@ class AppUsersController < ApplicationController
     def set_app_user
         @app_user = @app.app_users.find(params[:id]);
     end
-    def app_pamras
+    def app_params
         params[:app_user]
     end
 end
